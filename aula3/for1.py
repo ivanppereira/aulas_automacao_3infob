b54f515ef07b1f4bb48f39784aba3b57acda633d
Ivan Pereira <ivan.pereira@ifsuldeminas.edu.br>
2026-03-23 08:41:06 -0300

    Aula 3 : Estrutura de repetição, lista e discionários

---
+++ aula3/for1.py
@@ -0,0 +1,29 @@
+#Estrutura de Repetição For: Utilizado para repetir um conjunto
+#instruções - comandos, quando sabemos quantas vezes a instrução deve
+#ser repetida.
+
+#Exemplo 1
+#for variavel in range(valor inicial, valor final, passo)
+#range -> intervalo de valores
+
+
+
+#range(1, 10, 1): Intervalo Gerado -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+for i in range(1, 10, 1):
+    print(i, end=" ")
+
+print("\n")
+
+#range(1, 7, 2): Intervalo Gerado -> 1, 3, 5
+for i in range(1, 7, 2):
+    print(i, end=" ")
+print("\n")
+
+#range(0, -4, -1)        : Intervalo Gerado -> 0, -1, -2, -3
+for i in range(0, -4, -1):
+    print(i, end=" ")
+print("\n")
+
+#range(10) -> 0,1,2,3,4,5,6,7,8,9 
+for i in range(10):
+    print(i, end=" ")
---
+++ aula3/for2.py
@@ -0,0 +1,16 @@
+#Estrutura de repetição For: 
+#Percorrer listas
+#Lista estrutura de dados composta (armazena diversos valores)
+#Variável que possui vários valores
+
+nomes = ['Ivan', 'Gael', 'Mariana', 'Isis']
+print(nomes)
+
+#imprime todos os nomes da lista utilizando range
+for i in range(4):
+    print(nomes[i])
+
+#imprime todos os nomes usando interador de lista
+for nome in nomes:
+    print(nome)
+
