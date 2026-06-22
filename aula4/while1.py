309749490de5b0c4e44c97f2234350413c11fc7f
Ivan Pereira <ivan.pereira@ifsuldeminas.edu.br>
2026-06-22 08:50:19 -0300

    revisão

--- aula4/while1.py
+++
@@ -1,7 +0,0 @@
-#Estrutura de Repetição: While
-
-i = 10
-
-while i > 0:
-    print("Teste", i)
-    i = i - 1
--- aula5/exercicio_f1.py
+++
@@ -1,8 +0,0 @@
-''' f1
-Crie uma função chamada quadrado que recebe
-um número inteiro por parâmetro e retorna
-o quadrado desse número.
-'''
-
-def quadrado(n):
-    return n ** 2
